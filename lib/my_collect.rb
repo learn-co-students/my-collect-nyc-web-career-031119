66e4474270a3bc198c8eb796ca4efb17189df980
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-13 19:53:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
-
+def my_collect (collection)
+  array = []
+  if block_given?
+    i = 0
+    while i < collection.length
+      new_ele = yield(collection[i])
+      array << new_ele
+      i = i + 1
+    end
+    return array
+  end 
+end
