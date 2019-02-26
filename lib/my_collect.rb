d802f4fcc7d39b930e2f9ba7bbe590de367e7db1
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 21:38:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+require "pry"
 
-
+def my_collect(array)
+  counter = 0
+  new_array = []
+  
+  while counter < array.length
+    new_array.push(yield array[counter])
+    binding.pry
+    counter = counter + 1
+  end
+  
+  new_array
+end
